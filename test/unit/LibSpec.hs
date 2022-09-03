19a5ce5ea961fe21f93e8094030f5dbb824ea45e
William Zimmerman <will.zimmerman@gmail.com>
2022-09-03 18:09:20 -0400

    Converting HUnit tests to use Hspec

--- test/unit/LibSpec.hs
+++ test/unit/LibSpec.hs
@@ -3,25 +3,13 @@ module LibSpec
   ) where
 
 import           Lib
-import           Test.HUnit
 import           Test.Hspec
-import           Test.Hspec.Contrib.HUnit
-
-testToLetters :: Test
-testToLetters = TestCase
-  (assertEqual "toLetters with actual word"
-               [Letter 'c' 1, Letter 'a' 2, Letter 't' 3]
-               (toLetters "cat")
-  )
-
-testToLettersWithEmptyString :: Test
-testToLettersWithEmptyString =
-  TestCase (assertEqual "toLetters with empty string" [] (toLetters ""))
-
-testSuite :: Test
-testSuite = TestList [testToLetters, testToLettersWithEmptyString]
 
 spec :: Spec
 spec = do
-  describe "running legacy HUnit tests" $ do
-    fromHUnitTest testSuite
+  describe "testToLetters" $ do
+    it "should return empty list with empty string" $ do
+      toLetters "" `shouldBe` []
+
+    it "should return Letters in order with actual word" $ do
+      toLetters "cat" `shouldBe` [Letter 'c' 1, Letter 'a' 2, Letter 't' 3]
