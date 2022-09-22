cf1f0622b17b11ad607e56007ff032ba361f63ec
William Zimmerman <will.zimmerman@gmail.com>
2022-09-22 17:09:10 -0400

    First stab at CLI loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,41 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
+
+import           Lib
+import           System.IO
 
 main :: IO ()
 main = do
-    putStrLn "Hello world!"
+  hSetBuffering stdout NoBuffering
+  putStrLn "Welcome to Worlde!"
+  answer <- getAnswer
+  putStrLn "Enter guesses one at a time; :q to quit"
+  mainLoop answer
+
+getAnswer :: IO String
+getAnswer = do
+  putStr "Enter the answer: "
+  getLine
+
+mainLoop :: Answer -> IO ()
+mainLoop answer = do
+  putStr "> "
+  guess <- getLine
+-- TODO: Add quit
+  let evaluation = evaluateGuess answer guess
+  if guessIsCorrect evaluation
+    then do
+      putStrLn "You solved Wordle!"
+      return ()
+    else do
+      putStrLn (formatEval evaluation)
+      mainLoop answer
+
+formatEval :: [LetterEval] -> String
+formatEval = concatMap
+  (\(LetterEval _ char status) -> case status of
+    NotInAnswer       -> ['<',char,'>']
+    IncorrectPosition -> ['{',char,'}']
+    CorrectPosition   -> ['[',char,']']
+  ) 
