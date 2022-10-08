43ab3e4b9ac99ca041ebe52f083ec41092e9d5ab
William Zimmerman <will.zimmerman@gmail.com>
2022-10-08 09:36:17 -0400

    Committing steps before larger refactor

--- src/BotLib.hs
+++ src/BotLib.hs
@@ -15,3 +15,7 @@ allCorrectLettersMatch guessEval guess =
         guessEval
   in  all (`elem` guess) requiredLetters
 
+-- allLettersInIncorrectPositionAreInGuess :: GuessEval -> [Letter] -> Bool
+-- Identify all letters in incorrect position from GuessEval
+-- Remove all letters from guess that have matched to letters in correct position
+-- iterate over letters in incorrect position and match them to remaining letters in guess
