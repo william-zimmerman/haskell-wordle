90823686d926531b24e4428a09ab0c8968523f8d
William Zimmerman <will.zimmerman@gmail.com>
2024-07-05 17:53:00 -0400

    Displaying words on webpage

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 - [x] display hello world web page  
 - [x] rename wordle to wordle-cli  
 - [x] have application load list of words from source  
-- [ ] web application displays list of words  
+- [x] web application displays list of words  
 - [ ] application connects to a database  
 - [ ] display list of words from database  
 - [ ] pick a random word from list of possible words  
--- app/web/Main.hs
+++ app/web/Main.hs
@@ -20,17 +20,10 @@ main =
     runSpock 8080 (spock spockCfg app)
 
 readWords :: IO [String]
-readWords = do
-  entireFile <- readFile "words.txt"
-  let linesOfFile = words entireFile
-  putStrLn $ printf "read file with %d entries" (length linesOfFile)
-  return linesOfFile
+readWords = words <$> readFile "words.txt"
 
 app :: SpockM () MySession MyAppState ()
 app =
-  do get root $ text $ T.pack "Hello world!"
-
---    get ("hello" <//> var) $ \name ->
---        do (DummyAppState ref) <- getState
---           visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
---           text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+  do
+    (InitState allWords) <- getState
+    get root $ text $ T.pack (printf "Words: %s" (unwords allWords))
